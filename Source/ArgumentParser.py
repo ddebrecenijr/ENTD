dfe70f31f5fb3118a43a77e01f1c6b5f9efe0287
David Debreceni Jr <davidjr@debreceni.net>
2018-03-05 23:55:04 -0500

    Updated ArgumentParser

--- Source/ArgumentParser.py
+++ Source/ArgumentParser.py
@@ -10,6 +10,9 @@ class ArgParser:
         ap = argparse.ArgumentParser()
         ap.add_argument('-f', help='Name of PCAP File', required=True, nargs='?')
         ap.add_argument('-p', help='Ports', required=False, nargs='?')
+        ap.add_argument('-u', help='Update Domain JSON List', required=False, nargs='?')
+        ap.add_argument('-s', help='Number of Threads, Defaults to 10', required=False, nargs='?', type=int)
+        ap.add_argument('-i', help='Number of Domains to Analyze, Defaults to 10k', required=False, nargs='?', type=int)
 
         return ap.parse_args()
 
@@ -20,5 +23,8 @@ class ArgParser:
         """
         return dict(
             file=self.args.f,
-            ports=self.args.p
+            ports=self.args.p,
+            update=self.args.u or False,
+            threads=self.args.s or 10,
+            num_domains=self.args.i or 10000
         )
