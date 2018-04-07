0539f1e19c82db5fa5a5676ac5d9bae1e3818861
kazetomizu <31356720+kazetomizu@users.noreply.github.com>
2018-04-07 14:37:23 -0400

    Update Socket.py

--- Source/Facade/Socket.py
+++ Source/Facade/Socket.py
@@ -1,17 +1,39 @@
 import socket
 
-__author__ = "Sneh Patel"
+__author__ = "Sneh Patel, Bryan Chen"
 
 class Socket(object):
     """
     A Facade for the library Socket
     """
-    def __init__(self):
+    def __init__(self, sock=None):
         """
         Creates an INET and Streaming Socket.
         :param: None
         :return: Socket
         """
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(30)
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
 
+    def sock_convert_ip(self, ip):
+        try:
+            return socket.inet_ntop(socket.AF_INET, ip)
+        except ValueError:
+    return socket.inet_ntop(socket.AF_INET6, ip)
+
+
+    def socket(self, domain):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(30)
+
+        except(TimeoutError, socket.timeout):
+            print(f'{domain} connection attempt failed due to timeout.')
+        except socket.gaierror:
+            print(f'{domain} getaddrinfo failed.')
+        except ConnectionRefusedError:
+            print(f'{domain} refused connection.')
+        except ConnectionResetError:
+            print(f'{domain} connection forcibly closed.')
