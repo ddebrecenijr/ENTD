fb2ac2027f50f433a59753e1b5cc70e8cff2f356
sneh <33499703+tigerpatel13@users.noreply.github.com>
2018-04-06 21:33:09 -0400

    Added SSL to Facade Folder, added comments, fixed DomainExtrator to implement it

---
+++ Source/Facade/SSL.py
@@ -0,0 +1,43 @@
+import ssl
+from Source.Facade import Socket
+
+__author__ = "Sneh Patel"
+
+class SSL(object):
+    """
+    A Facade for the library SSL
+    """
+    def __init__(self):
+        pass
+
+    def SSL(self, domain):
+        """
+        Returns Dictionary containing info
+        :param domain: Domain Name
+        :return: Dictionary containing info
+        """
+        try:
+            ssl_sock = ssl.wrap_socket(Socket)
+            ssl_sock.connect((domain, 443))
+            
+            domain_info = {
+                    "source_ip": ssl_sock.getpeername()[0],
+                    "destination_ip": None, 
+                    "source_port": 443,
+                    "destination_port": None, 
+                    "version": ssl_sock.version(), 
+                    "selected_ciphersuite": ssl_sock.cipher()[0]
+                    }
+
+            ssl_sock.close()
+            return domain_info
+        except ssl.SSLError:
+            print(f'{domain} failed to connect, ssl error.')
+        except(TimeoutError, Socket.timeout):
+            print(f'{domain} connection attempt failed due to timeout.')
+        except Socket.gaierror:
+            print(f'{domain} getaddrinfo failed.')
+        except ConnectionRefusedError:
+            print(f'{domain} refused connection.')
+        except ConnectionResetError:
+            print(f'{domain} connection forcibly closed.')
